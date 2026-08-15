b79bbac3438dd2780d42305f06f625588df534d2
Dilnawaj Alam <mddilnawazsheikh0786@gmail.com>
2026-08-15 23:22:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Floyd's triangle/floyds-triangle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // code here
+    int count = 1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<count++<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
