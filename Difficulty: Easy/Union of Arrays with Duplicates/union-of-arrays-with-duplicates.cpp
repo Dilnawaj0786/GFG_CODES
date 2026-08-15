fc755830077154c5fd23f5d1c9ef6a526f5e0487
Dilnawaj Alam <mddilnawazsheikh0786@gmail.com>
2026-08-15 23:28:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        // code here
+        int n = a.size();
+        int m = b.size();
+
+        int i = 0;
+        int j = 0;
+
+        while (i < n && j < m) {
+
+            if (a[i] == b[j]) {
+                i++;
+                j++;
+            }
+            else if (a[i] < b[j]) {
+                i++;
+            }
+            else {
+                a.push_back(b[j]);
+                j++;
+            }
+        }
+
+        while (j < m) {
+            a.push_back(b[j]);
+            j++;
+        }
+
+        sort(a.begin(), a.end());
+
+        a.erase(unique(a.begin(), a.end()), a.end());
+
+        return a;
+    }
+};
