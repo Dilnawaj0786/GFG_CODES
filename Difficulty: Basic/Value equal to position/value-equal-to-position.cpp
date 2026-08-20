d7c5132e67cea9f4bb0f0ad8f8eae647fdf463d9
Dilnawaj Alam <mddilnawazsheikh0786@gmail.com>
2026-08-20 23:02:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value equal to position/value-equal-to-position.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<int> valEqualToPos(vector<int>& arr) {
+        // code here
+        vector<int> ans;
+        for(int i=0;i<arr.size();i++){
+            if(i+1 == arr[i]){
+                ans.push_back(arr[i]);
+            }
+        }
+        return ans;
+    }
+};
