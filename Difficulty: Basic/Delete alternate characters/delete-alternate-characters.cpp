ce620de382cec9fdb01e5dd6bc584a78589319c7
Dilnawaj Alam <mddilnawazsheikh0786@gmail.com>
2026-08-28 23:30:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Delete alternate characters/delete-alternate-characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    string delAlternate(string s) {
+        // code here
+        string ans = "";
+        for(int i=0;i<s.length();i++){
+            if(i % 2 == 0){
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
