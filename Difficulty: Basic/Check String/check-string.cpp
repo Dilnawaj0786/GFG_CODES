3e798b93a065133617b6c31468a1ffa92d92781f
Dilnawaj Alam <mddilnawazsheikh0786@gmail.com>
2026-08-28 23:33:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check String/check-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool check(string s) {
+        // code here.
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i] != s[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
