bb77b213c2848546329d0543c28bd735f0e97944
Dilnawaj Alam <mddilnawazsheikh0786@gmail.com>
2026-09-15 23:02:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue Reversal/queue-reversal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    void reverseQueue(queue<int> &q) {
+        // code here
+        stack<int>s;
+        while(!q.empty()){
+            s.push(q.front());
+            q.pop();
+        }
+        while(!s.empty()){
+            q.push(s.top());
+            s.pop();
+        }
+    }
+};
