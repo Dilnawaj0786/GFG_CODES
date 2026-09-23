4ea98e6be74fdcc15c350b0a386e251afb6b29e1
Dilnawaj Alam <mddilnawazsheikh0786@gmail.com>
2026-09-23 12:48:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    char firstRep(string s) {
+        // code here.
+        vector<int> freq(26,0);
+        for(char ch : s){
+            freq[ch-'a']++;
+        }
+        
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]-'a'] > 1){
+                return s[i];
+            }
+        }
+        return '#';
+    }
+};
